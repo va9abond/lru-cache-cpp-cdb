989555f066cef2c99ed42bdd09637a7887b6b55f
Rustem Sirazetdinov <avesirazetdinov@gmail.com>
2024-01-05 21:57:19 +0300

    checkout: remove size_ from lru_cache

--- lru.h
+++ lru.h
@@ -17,23 +17,23 @@ namespace alias {
 
 namespace caches {
 
-    template <typename key_ty, typename ty, int tsize>
+    template <typename key_ty, typename ty, size_t cache_size>
     class lru_cache {
     public:
         using key_type    = key_ty; // cache_key_type
-        using mapped_type = ty;  // cache_mapped_type
+        using mapped_type = ty; // cache_mapped_type
         using reference   = mapped_type&; // cache_reference
         using value_type  = std::pair<const key_ty, ty>; // cache_value_type
 
-        using iterator = std::list<value_type>::iterator;
+        using iterator = typename std::list<value_type>::iterator;
 
     private:
         using hash_key_type    = key_type;
-        using hash_mapped_type = std::list<value_type>::iterator;
+        using hash_mapped_type = typename std::list<value_type>::iterator;
         using hash_value_type  = std::pair<const hash_key_type, hash_mapped_type>;
 
     public:
-        lru_cache() : size_(tsize), cache_(0), hash_(0) {}
+        lru_cache() : cache_(0), hash_(0) {}
 
         lru_cache (const lru_cache&) = delete;
         lru_cache& operator= (const lru_cache&) = delete;
@@ -47,7 +47,7 @@ namespace caches {
             // check PRECOND
             assert(hash_.find(new_key) == hash_.end());
 
-            if (cache_.size() == size_) { // cache is full
+            if (cache_.size() == cache_size) { // cache is full
                 // remove lowest priority key before
                 hash_.erase(hash_.find( cache_.back().first ));
                 cache_.pop_back();
@@ -59,6 +59,10 @@ namespace caches {
             hash_.insert({new_key, cache_loc});
 
             return cache_loc;
+        }
+
+        bool full() const {
+            return cache_.size() == cache_size;
         }
 
         // check does key in cache
@@ -98,7 +102,6 @@ namespace caches {
         }
 
     private:
-        const int size_;
         std::list<value_type> cache_;
         alias::unmap<
             hash_key_type,
